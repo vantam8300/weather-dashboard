5d3a8add69dd88a15a9123926e9e1d5d9c7c4ada
vantam8300 <vantam8300@gmail.com>
2022-10-18 18:22:38 -0700

    finish calling api

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,40 @@
-var APIKey = "6e0d48ab44b7aa9166f2a5bc381f27d6";
+let APIKey = "6e0d48ab44b7aa9166f2a5bc381f27d6";
 
-var city = $("#city");
+let city = $("#city");
 
-var form = $(".project-form");
+let form = $(".project-form");
 
+let coordinate= {}
 
+// get latitude and longitude given city name
+function getLatnLon(cityName) {
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
 
-function handleSubmitForm(event) {
-    event.preventDefault();
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=" + APIKey;
     fetch(queryURL)
     .then(function(response){
         return response.json();
     }).then(function (data) {
-        console.log(data)
+        let lon = data.coord.lon;
+        let lat = data.coord.lat;
+        let queryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" 
+        + lat + "&lon=" + lon + "&appid=" +APIKey;
+        return fetch(queryURL);
+    }).then(function(response){
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
     });
+}
+
+
+function handleSubmitForm(event) {
+    event.preventDefault();
+
+    //get city name
+    let cityName = city.val();
+    // get Lat and Lon
+    getLatnLon(cityName);
+   
 
 
 }
